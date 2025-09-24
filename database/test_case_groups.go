27032045009639556e573386cf59b776ec6d0577
leonid <leonid@computer.local>
2025-09-24 21:04:59 +0700

    Вывод статуса тест-кейсов в виде текста

--- database/test_case_groups.go
+++ database/test_case_groups.go
@@ -20,10 +20,8 @@ func AddTestCaseGroup(name string, user *models.User) (*models.TestCaseGroup, er
 	}
 
 	res, err := GetDB().Exec(
-		`
-		INSERT INTO test_case_groups (uuid, status, name, creator)
-		VALUES (?, ?, ?, ?)
-		`,
+		`INSERT INTO test_case_groups (uuid, status, name, creator)
+		VALUES (?, ?, ?, ?)`,
 		tcg.UUID, tcg.Status, tcg.Name, tcg.Creator,
 	)
 
@@ -45,20 +43,22 @@ func GetTestCaseGroups() *[]models.TestCaseGroupResponse  {
 	var result []models.TestCaseGroupResponse
 	err := GetDB().Select(
 		&result,
-		`
-		SELECT
+		`SELECT
 			test_case_groups.id,
 			test_case_groups.uuid,
 			test_case_groups.name,
 			test_case_groups.status,
 			users.login AS creator
 		FROM test_case_groups
-		JOIN users ON test_case_groups.creator = users.uuid
-		`,
+		JOIN users ON test_case_groups.creator = users.uuid`,
 	)
 
 	if err != nil {
 		panic(err)
+	}
+
+	for i, entry := range result {
+		result[i].Status = entry.TestCaseGroup.Status.Name()
 	}
 
 	return &result
